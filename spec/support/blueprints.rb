a9c30ac07388e4b9b7341e016cde6968799ac406
Brent Chuang <burennto@gmail.com>
2011-08-01 03:00:12 +1000

    Improved the User show page and added a Gravatar

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,7 +1,7 @@
 require 'machinist/active_record'
 
 User.blueprint do
-  name                  { "User" }
+  name                  { "User's Name" }
   email                 { "user@example.com" }
   password              { "foobar" }
   password_confirmation { "foobar" }
