12a0f8ce0135d32edda4a72cf7d7a4b44ce6a31a
Brent Chuang <burennto@gmail.com>
2011-07-31 19:05:03 +1000

    Added a Users controller and layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ RailsTutorial::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   
+  match "/signup",  :to => 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
