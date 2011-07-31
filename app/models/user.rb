f302ac8e7d1c6a9616467dfcc0f192d8a2759ccf
Brent Chuang <burennto@gmail.com>
2011-07-31 23:34:32 +1000

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
   
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => EMAIL_REGEX },
+                    :uniqueness => { :case_sensitive => false }
 end
