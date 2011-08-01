f2a59ad7d5bfb412555d9a99aecc41371e3c38e4
Brent Chuang <burennto@gmail.com>
2011-08-02 02:00:58 +1000

    Completed signin/out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,4 +30,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   
   config.before(:each) { Machinist.reset_before_test }
+  
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
